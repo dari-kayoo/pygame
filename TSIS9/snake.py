83d73efa70ef82537e068fae4eb8cf3bbbc964be
dari-kayoo <78098445+dari-kayoo@users.noreply.github.com>
2021-04-19 23:08:54 +0600

    without bags

--- TSIS9/snake.py
+++ TSIS9/snake.py
@@ -7,9 +7,8 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill((175, 134, 246))
 pygame.display.set_caption('Snake')
 
-
-class Fruit():
-    def __init__(self, x=-200, y=-200, color=(255, 255, 255), width=16, height=16):
+class Food():
+    def __init__(self, x=0, y=0, color=(255, 255, 255), width=16, height=16):
         self.x = x
         self.y = y
         self.color = color
@@ -21,25 +20,26 @@ class Fruit():
         pygame.draw.rect(screen, self.color,
                          (self.x, self.y, self.width, self.height))
         self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
-        pygame.draw.rect(screen, [0, 255, 0],
-                         (self.x, self.y, self.width, self.height), 1)
 
 
 class Snake():
     def __init__(self):
-        self.x = 128
-        self.y = 128
+        self.x = -1
+        self.y = 50
         self.speed = 2
         self.velocity = [self.speed, 0]  # dx,dy
         self.size = 1
         self.radius = 8
         self.hitbox = pygame.Rect(
             self.x-self.radius, self.y-self.radius, self.radius*2, self.radius*2)
+
         self.elements = [[self.x, self.y, self.hitbox]]
+
         self.up = False
         self.down = False
         self.right = True
         self.left = False
+
         for i in range(10):
             self.add_size()
 
@@ -50,15 +50,14 @@ class Snake():
         self.elements.append([self.x, self.y, self.hitbox])
 
     def draw(self):
-        for element in self.elements:
-            pygame.draw.circle(screen, ((
-                3*element[0]) % 256, 100, (element[0]+element[1]) % 256), element[0:2], self.radius)
+        for i in self.elements:
+            
             self.hitbox = pygame.Rect(
-                element[0]-self.radius, element[1]-self.radius, self.radius*2, self.radius*2)
-            ind = self.elements.index(element)
+                i[0]-self.radius, i[1]-self.radius, self.radius*2, self.radius*2)
+            ind = self.elements.index(i)
             self.elements[ind][2] = self.hitbox
-            pygame.draw.rect(screen, [255, 2, 255], (element[0]-self.radius,
-                             element[1]-self.radius, self.radius*2, self.radius*2), 1)
+            pygame.draw.circle(screen, (148, 12, 100),
+                               (i[0], i[1]), self.radius)
 
     def move(self):
         for i in range(1, self.size):
@@ -68,14 +67,14 @@ class Snake():
         self.elements[0][1] += self.velocity[1]
 
 
-def spawn_fruit():
-    while len(fruits) < 8:
+def spawn():
+    while len(foods) < 8:
         x_ = random.randrange(64, 600, 10)
         y_ = random.randrange(64, 600, 32)
         color_ = (random.randrange(100, 255), random.randrange(
             100, 255), random.randrange(100, 255))
         width_ = height_ = random.randrange(8, 24, 4)
-        fruits.append(Fruit(x_, y_, color_, width_, height_))
+        foods.append(Food(x_, y_, color_, width_, height_))
 
 
 def draw_score():
@@ -90,12 +89,12 @@ def initializing():
     draw_score()
     snake.draw()
     snake.move()
-    for fruit in fruits:
-        fruit.draw()
-        if snake.elements[0][2].colliderect(fruit.hitbox):
+    for f in foods:
+        f.draw()
+        if snake.elements[0][2].colliderect(f.hitbox):
             score += 1
             snake.add_size()
-            fruits.remove(fruit)
+            foods.remove(f)
 
 
 FPS = 60
@@ -103,7 +102,7 @@ clock = pygame.time.Clock()
 level_num = 1
 score = 0
 walls = []
-fruits = []
+foods = []
 snake = Snake()
 run = True
 while run:
@@ -114,8 +113,11 @@ while run:
     for i in range(0, WIDTH, 10):
         pygame.draw.rect(screen, (255, 255, 12), (i, 0, 10, 10), 1)
         pygame.draw.rect(screen, (255, 255, 12), (i, WIDTH - 10, 10, 10), 1)
-        pygame.draw.rect(screen, (255, 255, 12), (0, i, 10, 10), 1)
+        pygame.draw.rect(screen, (175, 134, 246), (0, 50, 10, 10), 1)
+        pygame.draw.rect(screen, (175, 134, 246), (0, 40, 10, 10), 1)
+        pygame.draw.rect(screen, (255, 255, 12), (0, i , 10, 10), 1)
         pygame.draw.rect(screen, (255, 255, 12), (HEIGHT - 10, i, 10, 10), 1)
+
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d] and not snake.left:
         snake.right = True
@@ -134,7 +136,7 @@ while run:
         snake.up = snake.right = snake.left = False
         snake.velocity = [0, snake.speed]
     initializing()
-    spawn_fruit()
+    spawn()
     clock.tick(FPS)
     pygame.display.update()
 pygame.quit()
